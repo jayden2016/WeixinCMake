487fab6b1d7f66104ca1654620e3433d42df6d31
lvzongchao <lvzongchao@70mai.com>
2022-01-25 10:13:59 +0800

    fix bug
    Signed-off-by: lvzongchao <lvzongchao@70mai.com>

---
+++ build/weixin_port/cmake_install.cmake
@@ -0,0 +1,58 @@
+# Install script for directory: /mnt/e/watch/WeixinCMake_Project/WeixinCMake/weixin_port
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/mnt/e/watch/WeixinCMake_Project/WeixinCMake/build")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/mnt/e/watch/WeixinCMake_Project/WeixinCMake/build/lib/libWeixinChannel.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/mnt/e/watch/WeixinCMake_Project/WeixinCMake/build/lib" TYPE STATIC_LIBRARY FILES "/mnt/e/watch/WeixinCMake_Project/WeixinCMake/build/lib/libWeixinChannel.a")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/mnt/e/watch/WeixinCMake_Project/WeixinCMake/build/include/AdamClientChannelImpl.h;/mnt/e/watch/WeixinCMake_Project/WeixinCMake/build/include/adam_channel_common_define.h;/mnt/e/watch/WeixinCMake_Project/WeixinCMake/build/include/adam_client_channel_zidl.h;/mnt/e/watch/WeixinCMake_Project/WeixinCMake/build/include/weixin_channel.h")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/mnt/e/watch/WeixinCMake_Project/WeixinCMake/build/include" TYPE FILE FILES
+    "/mnt/e/watch/WeixinCMake_Project/WeixinCMake/weixin_port/./include/AdamClientChannelImpl.h"
+    "/mnt/e/watch/WeixinCMake_Project/WeixinCMake/weixin_port/./include/adam_channel_common_define.h"
+    "/mnt/e/watch/WeixinCMake_Project/WeixinCMake/weixin_port/./include/adam_client_channel_zidl.h"
+    "/mnt/e/watch/WeixinCMake_Project/WeixinCMake/weixin_port/./include/weixin_channel.h"
+    )
+endif()
+
